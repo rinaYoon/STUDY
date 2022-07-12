d164bd499f0957332910ab5acc5f57a2e0dc28d6
rinaYoon <ynarina@cttd.co.kr>
2022-07-12 18:02:28 +0900

    css, js작업

--- 03_mamonde/js/main.js
+++ 03_mamonde/js/main.js
@@ -34,3 +34,24 @@ var swiper = new Swiper(".section01 .swiper", {
     prevEl: ".section01 .button-arrow-prev",
   },
 });
+
+// section02 - 이전, 다음으로 넘어갈때 text-wrap 이 안움직임
+var section02Swiper = new Swiper(".section02 .swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 15,
+  loop: true,
+  pagination: {
+    el: ".section02 .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".section02 .button-arrow-next",
+    prevEl: ".section02 .button-arrow-prev",
+  },
+});
+section02Swiper.on('slideNextTransitionStart', function(){
+  $('.section02 .swiper-slide-active .text-wrap').animate({left:500},500);
+});
+section02Swiper.on('slideResetTransitionStart', function(){
+  $('.section02 .swiper-slide-active .text-wrap').animate({right:500},500);
+});
