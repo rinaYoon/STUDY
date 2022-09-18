d18afb48b1a181e8d7ad327943ceaf54b2311633
rinaYoon <ynarina@cttd.co.kr>
2022-09-18 18:54:40 +0900

    js설정추가

--- 04_game/play.js
+++ 04_game/play.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 
   // 버튼을 클릭하면 3초 반복이미지 중단
   var userButton = $('.user .tool .tool__button');
-  var userButtonSet = {
+  var userButtonControl = {
     off: function(){
       userButton.attr("disabled", "disabled");
       userButton.css("cursor", "default");
@@ -30,14 +30,21 @@ $(document).ready(function(){
   }
 
   userButton.click(function() {
-    userButtonSet.off();
+    userButtonControl.off();
+
     clearInterval(roopTime);
+
     setTimeout(function(){
       roopTime = setInterval(imgRoop, 100);
-      userButtonSet.on();
-    },2000);
+      userButtonControl.on();
+      $('.text-box .text').text("가위! 바위! 보!");
+      //위에 text 있는 이유 : 가위바위보 결과값 이후 기본멘트로 초기화
+    },3000);
   });
   
+
+  // 비교하기, 결과값을 폰트에 나타내기
+  // (setTimeout의 시간안에 나오고 사라져야함)
 
 
 
