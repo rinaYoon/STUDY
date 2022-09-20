56a5a49b7075492735dd61b5e92dadfc0ce599e3
rinaYoon <ynarina@cttd.co.kr>
2022-09-20 18:10:45 +0900

    변수a로 결과값나오게 함

--- 04_game/play.js
+++ 04_game/play.js
@@ -1,26 +1,29 @@
 $(document).ready(function(){
 
   // 이미지 반복
-  var i = 0;
-  var images = $('.system .tool .system__img')
+  let i = 0;
+  let a = 0;
+  let images = $('.system .tool .system__img')
 
-  var imgRoop = function(){
+  let imgRoop = function(){
     images.hide();
     images.eq(i).show();
+    a = i;
     i++;
     if(i >= images.length){
       i = 0;
     }
   }
 
-  var roopTime = setInterval(imgRoop, 100);
+  let roopTime = setInterval(imgRoop, 2000);
 
-  console.log();
+  // 콘솔 a
+  console.log(a);
 
 
   // 버튼을 클릭하면 3초 반복이미지 중단
-  var userButton = $('.user .tool .tool__button');
-  var userButtonControl = {
+  let userButton = $('.user .tool .tool__button');
+  let userButtonControl = {
     off: function(){
       userButton.attr("disabled", "disabled");
       userButton.css("cursor", "default");
@@ -40,7 +43,7 @@ $(document).ready(function(){
     clearInterval(roopTime);
 
     setTimeout(function(){
-      roopTime = setInterval(imgRoop, 100);
+      roopTime = setInterval(imgRoop, 2000);
       userButtonControl.on();
       userButtonControl.cssBorderRemove();
       //가위바위보 결과값 이후 기본멘트로 초기화
@@ -51,13 +54,16 @@ $(document).ready(function(){
     // 클릭한 버튼과 출력한 이미지 비교하기
     if(userButton.index(this) == 0){
       $('.text').text('가위 누름');
-      //만약에 현재 노출되고있는 이미지가 이미지index값 이라면
-      //이미지의 index값을 구해야함
+      //만약에 현재 노출되고있는 이미지가 몇번째 라면?
+      //이미지.eq(?)값 구하기
     }else if(userButton.index(this) == 1){
       $('.text').text('바위 누름');
     }else if(userButton.index(this) == 2){
       $('.text').text('보 누름');
     }
+
+  // 콘솔 a
+  console.log(a);
 
   });
 
@@ -72,19 +78,7 @@ $(document).ready(function(){
     userButtonControl.cssBorderRemove();
   });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
 
 
 
