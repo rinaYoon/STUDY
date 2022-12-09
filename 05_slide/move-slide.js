52a1a4193858b8af8e315804712440a97017af4c
rinaYoon <ynarina@cttd.co.kr>
2022-12-09 16:00:03 +0900

    이슈 대응 및 버튼 기능추가
    트랜지션이벤트리스너 이동+if문으로 컨트롤, 버튼 클릭 후 1.5초동안 텀생김, next슬라이드 도중 이전버튼 누를때 대응

--- 05_slide/move-slide.js
+++ 05_slide/move-slide.js
@@ -4,12 +4,16 @@ document.addEventListener("DOMContentLoaded",function(){
 });
 
   const slideWrapper = document.querySelector('.slide-container .slide-wrapper');
+  const buttonWrap = document.querySelector(".slide-container .slide-controls");
+  const prevButton = document.querySelector(".slide-container .button__prev");
+  const nextButton = document.querySelector(".slide-container .button__next");
   let duration = 3000;
   let restartTimer = null;
   let autoTimer = null;
   let animationTime = null;
-  let played = true;
+  let played = null;
   let playedTimer = null;
+  let slideNextControl = null;
 
 
   function init(){
@@ -60,7 +64,6 @@ document.addEventListener("DOMContentLoaded",function(){
     let updateSlideLength = updateSlide.length;
     let initPosition = 100 / updateSlideLength;
 
-    slideWrapper.classList.remove('slideNextControl');
     removeAnimation();
     slideWrapper.style.transform = "translateX(" + -initPosition + "%)"
     slideWrapper.prepend(updateSlide[updateSlideLength - 1]);
@@ -76,54 +79,54 @@ document.addEventListener("DOMContentLoaded",function(){
     let updateSlideLength = updateSlide.length;
     let initPosition = 100 / updateSlideLength;
 
-    slideWrapper.style.transform = "translateX(" + -initPosition + "%)"
-    slideWrapper.classList.add('slideNextControl');
-    console.log('A');
+    slideWrapper.style.transform = "translateX(" + -initPosition + "%)";
+    slideNextControl = true;
+    //console.log('A___'+slideNextControl);
   }
 
   function slideNextEventContent(){
-    if(slideWrapper.classList.contains('slideNextControl')){
+    if(slideNextControl === true){
       let updateSlide = document.querySelectorAll(".slide-container .slide__item");
 
-      slideWrapper.classList.remove('slideNextControl');
+      slideNextControl = false;
       removeAnimation();
       clearTimeout(animationTime);
       slideWrapper.style.transform = "translateX(" + 0 + "%)";
       slideWrapper.appendChild(updateSlide[0]);
       animationTime = setTimeout(addAnimation, 100);
-      console.log('B'); 
+      //console.log('B___'+slideNextControl); 
     }
   }
 
+
+
+  // Next슬라이드전용 슬라이드들 위치 바꾸는 이벤트
   slideWrapper.addEventListener('transitionend', slideNextEventContent);
-
-
-
-
-  const buttonWrap = document.querySelector(".slide-container .slide-controls");
-  const prevButton = document.querySelector(".slide-container .button__prev");
-  const nextButton = document.querySelector(".slide-container .button__next");
 
   buttonWrap.addEventListener("click", function(e){
     pause();
     rePlay();
+    
+    //버튼제어장치, 한번클릭하면 1.5초동안 클릭X
+    if(played === true){
+      played = false;
+      playedTimer = setTimeout(function(){
+        played = true;
+      } ,1500); 
 
-    if(played){
       if(e.target === prevButton){
-        console.log('prev click');
-        slidePrev();
-        played = false;
-        playedTimer = setTimeout(function(){
-          played = true;
-        } ,1500); //버튼제어장치
-
-      }else if(e.target === nextButton){
-        console.log('naxt click');
+        //console.log('prev click___'+slideNextControl);
+        
+        //Next슬라이드 이동 중에 Prev버튼을 눌렀을때의 대응
+        if(slideNextControl === true){
+          slideWrapper.style.transform = "translateX(" + 0 + "%)";
+          slideNextControl = false;
+        }else{
+          slidePrev();
+        }
+      }else if(e.target === nextButton){ 
+        //console.log('naxt click');
         slideNext();
-        played = false;
-        playedTimer = setTimeout(function(){
-          played = true;
-        } ,1500); //버튼제어장치
       }
     }
   });
