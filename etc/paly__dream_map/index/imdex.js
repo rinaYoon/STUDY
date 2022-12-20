eac435e64d842476f3cf8b0c7416aa3074667e16
rinaYoon <ynarina@cttd.co.kr>
2022-12-20 16:58:54 +0900

    dream_map 로그인 스크립트 완료

--- etc/paly__dream_map/index/imdex.js
+++ etc/paly__dream_map/index/imdex.js
@@ -1,66 +1,48 @@
-// document.addEventListener("DOMContentLoaded", function(){
-
-// });
-
+const infoText = document.querySelector(".section-info .text");
 const loginForm = document.querySelector(".login-form");
 const submitButton = document.querySelector(".submit-button");
 const accountInput = document.querySelector("#account");
 const mapCodeInput = document.querySelector("#mapCode");
 
 let loginAccount = ["aaa", "bbb", "ccc", "ddd", "eee"];
-let mapLink = ["map-test.html", "map-test02.html"]
+let mapLink = ["map-test", "map-test02"];
 
-/*
-  1. input에서 입력한 value값이 위의 array안에 있는 값중 1개라도 일치하다면 addMapLink(); 함수실행.
-  2. 그 외에 input에서 입력한 value값이 하나도 다 안맞으면 alert 뜸.(마지막에한번만)
+function loginCheck(){
+  if (loginAccount.includes(accountInput.value)) {
+    mapCheak();
 
-  문제 1. 첫번째값 ("aaa")는 무조건 잘 되지만,
-  두번째값부터 ("bbb" ~ "eee") 입력하면 alert가 뜸.
-
-  문제 2. else 부분을
-  else if(!accountInput.value == loginAccount[i]) 이걸로 바꾼 후 위의 array가 아닌
-  다른 걸로 입력 시 alert가 뜨지 않음.
-
-*/
-
-function loginCheck(){ 
-  for (let i = 0; i < loginAccount.length; i++) {
-
-    //문제 1
-    // if (accountInput.value == loginAccount[i]) {
-    //   addMapLink();
-    //   return;
-
-    // }else{
-    //   alert('계정이 일치하지 않습니다.');
-    //   return;
-    // }
-
-    //문제 2
-    if (accountInput.value == loginAccount[i]) {
-      addMapLink();
-      return;
-
-    }else if(!accountInput.value == loginAccount[loginAccount.length - 1]){
-      alert('계정이 일치하지 않습니다.');
-      return;
+  }else{
+    if(accountInput.value === ""){
+      alert("계정을 입력하십시오.");
+    }else{
+      alert("계정이 일치하지 않습니다.");
     }
   }
+}
+
+function mapCheak(){
+    if (mapLink.includes(mapCodeInput.value)) {
+      addMapLink();
+
+    }else{
+      if(mapCodeInput.value === ""){
+        alert("코드를 입력하십시오.");
+      }else{
+        alert("코드가 일치하지 않습니다.");
+      }
+    }
 }
 
 function addMapLink(){
-  for (let i = 0; i < mapLink.length; i++) {
-    if (mapCodeInput.value == mapLink[i]) {
-      alert(mapCodeInput.value);
-      return;
-    }else{
-      alert('코드가 일치하지 않습니다.');
-      return;
-    }
-  }
+  loginForm.setAttribute("action", "../map/"+mapCodeInput.value+".html");
+  infoText.innerHTML = `계정 및 코드가 확인되었습니다...<br>접속하기 버튼을 눌러 이동하십시오.`;
+  submitButton.innerText = "접속하기";
+  loginForm.removeEventListener("submit", addButtonEvent);
 }
 
-loginForm.addEventListener("submit", function(event){
+function addButtonEvent(event){
   event.preventDefault();
   loginCheck();
-});
+}
+
+loginForm.addEventListener("submit", addButtonEvent);
