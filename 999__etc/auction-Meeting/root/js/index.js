96994517d1503cd6c0deb0a558abe79b98be43e6
rinaYoon <ynarina@cttd.co.kr>
2023-01-20 15:33:45 +0900

    좌담회 PC 완료

--- 999__etc/auction-Meeting/root/js/index.js
+++ 999__etc/auction-Meeting/root/js/index.js
@@ -1,12 +1,15 @@
 const redioSelect = document.querySelector(".slot-form--03 .select-list");
 const consentButton = document.querySelectorAll(".slot-form--03 .radio-select input")[0];
 const disagreeButton = document.querySelectorAll(".slot-form--03 .radio-select input")[1];
-const dimdBg = document.querySelector(".division-alert .dimd");
-const alertType = document.querySelectorAll(".division-alert .alert-type");
+
+const dimdBg = document.querySelector(".event-alert .dimd");
+const alertType = document.querySelectorAll(".event-alert .alert-type");
+
+const alertClossButton = document.querySelectorAll(".event-alert .alert-link");
 
 function consent(){
   redioSelect.addEventListener('click', function(e){
-    dimd();
+    alertCloss();
 
     if(e.target === consentButton){
       alertType[0].style = "";
@@ -19,10 +22,17 @@ function consent(){
   });
 }
 
-function dimd(){
-  dimdBg.style = "";
+function alertCloss(){
+  for (let i = 0; i < alertClossButton.length; i++) {
+    alertClossButton[i].addEventListener('click', function(e){
+      e.preventDefault();
+      
+      alertType[i].style.display = "none";
+    });
+  }
 }
 
+
 document.addEventListener("DOMContentLoaded",function(){
-  consent();
+  consent();  
 });
