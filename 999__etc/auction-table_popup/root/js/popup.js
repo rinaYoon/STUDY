f69eb4eaf7eeb28e77f31ac8f690bc0707cd7069
rinaYoon <ynarina@cttd.co.kr>
2023-02-21 14:29:22 +0900

    css일부수정, js 체크박스완료

--- 999__etc/auction-table_popup/root/js/popup.js
+++ 999__etc/auction-table_popup/root/js/popup.js
@@ -2,18 +2,20 @@
 해야하는거..
 
 - NO. 갯수만큼 숫자표기 (OK)
-- 체크박스 2개부터 item-button__once로 바뀜
+- 체크박스 2개부터 item-button__once로 바뀜 (OK)
 
 - 타입, 상태에 따라 사용부분이랑 그룹으로 바뀌는거
   - find대체용 찾기, 자식선택찾기
   - for또는 forEach 필요할듯
 
-- item-bundle : 쿠폰상태 문구 사용가능으로 변경, 체크박스 checked onclick="return false;" 삭제
-
-- __desable : 쿠폰상태 문구 '사용불가'변경, 체크버튼 checked onclick="return false;" 추가
-- __used : 쿠폰상태 문구 '사용완료'변경, 체크버튼 checked onclick="return false;" 추가
-- __typegroup : 쿠폰타입 문구 '그룹'변경 + type-num 추가
+- .item-bundle : 사용가능, lable을 black변경, 버튼__desable을 none변경
+- .item-bundle__desable : 사용불가, lable을 none변경, 버튼__desable을 black변경
+- .item-bundle__used : 사용완료, lable을 none변경, 버튼__desable을 black변경
+- .item-bundle__typegroup : '그룹' 변경 + type-num 추가
 */
+
+let count = 0;
+
 
 const number = document.querySelectorAll('[class^="area-"] .slot-item tbody .item-number .number');
 function numberIndex(){
@@ -22,12 +24,40 @@ function numberIndex(){
   }
 }
 
+const checkButton = document.querySelectorAll('[class^="area-"] .slot-item tbody input[type="checkbox"]');
+const itemButton = document.querySelectorAll('[class^="area-"] .division-coupon .item-button');
+const itemButtonOnes = document.querySelectorAll('[class^="area-"] .division-coupon .item-button__once');
+function itemCheckBox(){
+  for (let i = 0; i < checkButton.length; i++) {
+
+    checkButton[i].addEventListener('click', function(){
+
+      if(checkButton[i].checked === true){
+        count ++;
+        if(count > 1){
+          itemButton[0].style.display = "none";
+          itemButtonOnes[0].style.display = "block";
+        }
+      }else if(checkButton[i].checked === false){
+        count --;
+        if(count < 2){
+          itemButton[0].style.display = "block";
+          itemButtonOnes[0].style.display = "none";
+        }
+      }
+    });
+  }
+}
+
+
+
 
 
 
 
 document.addEventListener("DOMContentLoaded",function(){
   numberIndex();
+  itemCheckBox();
 });
 
 
