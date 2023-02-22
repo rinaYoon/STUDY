64f9c5cd83a576db1ca1601d40bc708aa1206b6e
rinaYoon <ynarina@cttd.co.kr>
2023-02-22 14:15:12 +0900

    타입별 상태완료, 정리하기

--- 999__etc/auction-table_popup/root/js/popup.js
+++ 999__etc/auction-table_popup/root/js/popup.js
@@ -4,13 +4,9 @@
 - NO. 갯수만큼 숫자표기 (OK)
 - 체크박스 2개부터 item-button__once로 바뀜 (OK)
 
-- 타입, 상태에 따라 사용부분이랑 그룹으로 바뀌는거
-  - find대체용 찾기, 자식선택찾기
-  - for또는 forEach 필요할듯
-
-- .item-bundle : 사용가능, lable을 black변경, 체크버튼__desable을 none변경
-- .item-bundle__desable : 사용불가, lable을 none변경, 체크버튼__desable을 black변경
-- .item-bundle__used : 사용완료, lable을 none변경, 체크버튼__desable을 black변경
+- .item-bundle : 사용가능, lable을 black변경, 체크버튼__desable을 none변경 (OK)
+- .item-bundle__desable : 사용불가, lable을 none변경, 체크버튼__desable을 black변경 (OK)
+- .item-bundle__used : 사용완료, lable을 none변경, 체크버튼__desable을 black변경 (OK)
 - .item-bundle__typegroup : '그룹' 변경 + type-num 추가
 */
 
@@ -24,9 +20,22 @@ const itemButton = document.querySelectorAll('[class^="area-"] .division-coupon 
 const itemButtonOnes = document.querySelectorAll('[class^="area-"] .division-coupon .item-button__once');
 
 const bundleInit = document.querySelectorAll('[class^="area-"] .slot-item tbody .item-bundle');
+const bundleInitStatus = document.querySelectorAll('[class^="area-"] .item-bundle .status');
+const bundleInitLable = document.querySelectorAll('[class^="area-"] .item-bundle label');
+const bundleInitType = document.querySelectorAll('[class^="area-"] .item-bundle .type');
+
 const bundleDesable = document.querySelectorAll('[class^="area-"] .slot-item tbody .item-bundle__desable');
+const bundleDesableStatus = document.querySelectorAll('[class^="area-"] .item-bundle__desable .status');
+const bundleDesableLable = document.querySelectorAll('[class^="area-"] .item-bundle__desable label');
+const bundleDesableType = document.querySelectorAll('[class^="area-"] .item-bundle__desable .type');
+
 const bundleUsed = document.querySelectorAll('[class^="area-"] .slot-item tbody .item-bundle__used');
+const bundleUsedStatus = document.querySelectorAll('[class^="area-"] .item-bundle__used .status');
+const bundleUsedLable = document.querySelectorAll('[class^="area-"] .item-bundle__used label');
+const bundleUsedType = document.querySelectorAll('[class^="area-"] .item-bundle__used .type');
+
 const bundleTypegroup = document.querySelectorAll('[class^="area-"] .slot-item tbody .item-bundle__typegroup');
+const bundleTypegroupType = document.querySelectorAll('[class^="area-"] .item-bundle__typegroup .type');
 
 let count = 0;
 
@@ -35,10 +44,33 @@ function couponInit(){
     numberIndex(i);
     itemCheckBox(i);
 
-    // 만약 bundleInit 가 있다면
     if(bundleInit[i]){
-      // innerHTML = '사용가능'
-      // lable을 black변경, 체크버튼__desable을 none변경
+      bundleInitStatus[i].innerHTML='사용가능';
+      bundleInitLable[i].style.display = "block";
+      bundleInitLable[i].nextElementSibling.style.display = "none";
+      bundleInitType[i].innerHTML='일반';
+      bundleInitType[i].nextElementSibling.style.display = "none";
+    }
+
+    if(bundleDesable[i]){
+      bundleDesableStatus[i].innerHTML='사용불가';
+      bundleDesableLable[i].style.display = "none";
+      bundleDesableLable[i].nextElementSibling.style.display = "block";
+      bundleDesableType[i].innerHTML='일반';
+      bundleDesableType[i].nextElementSibling.style.display = "none";
+    }
+
+    if(bundleUsed[i]){
+      bundleUsedStatus[i].innerHTML='사용완료';
+      bundleUsedLable[i].style.display = "none";
+      bundleUsedLable[i].nextElementSibling.style.display = "block";
+      bundleUsedType[i].innerHTML='일반';
+      bundleUsedType[i].nextElementSibling.style.display = "none";
+    }
+
+    if(bundleTypegroup[i]){
+      bundleTypegroupType[i].innerHTML='그룹';
+      bundleTypegroupType[i].nextElementSibling.style.display = "block";
     }
   }
 }
