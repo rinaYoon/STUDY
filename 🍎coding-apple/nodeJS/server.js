b478c7694606ec1b2bf59104753d1c94670f9aeb
rinaYoon <ynarina@cttd.co.kr>
2024-12-23 16:31:17 +0900

    재세팅

---
+++ 🍎coding-apple/nodeJS/server.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+1.항상 노드JS 버전 확인하기
+	(참고: https://github.com/rinaYoon/STUDY/blob/main/999__record/shell%2Bnode/fnm.md)
+
+2.프로젝트 생성하기 (package.json)
+	npm init -y
+
+3.라이브러리 세팅하기
+	npm install 라이브러리이름
+	(ex: npm install express, npm install nodemon)
+	(ex2: express는 서버열게도움주는거, nodemon은 저장후 바로 반영해주는거)
+*/
